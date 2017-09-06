f3c8fd3033299b2f9cdc3d36f51a6fa36d5dfcab
Isgi Arriza <isgiarriza@gmail.com>
2017-09-06 11:27:34 +0700

    4. Mini Calculator

--- js/main.js
+++ js/main.js
@@ -1,32 +1,12 @@
-//arithmatic operation
+//ambil value dari firstNumber
+var firstNumber = document.getElementById('firstNumber')
+var secondNumber = document.getElementById('secondNumber')
 
-function add(firstNumber, secondNumber){
-  return firstNumber + secondNumber;
+function add(){
+  var firstNumberInt = Number(firstNumber.value);
+  var secondNumberInt = Number(secondNumber.value);
+  var result = firstNumberInt + secondNumberInt;
+
+  //passing value to dom
+  document.getElementById("result").innerHTML = result;
 }
-
-function substract(firstNumber, secondNumber){
-  return firstNumber - secondNumber;
-}
-
-function multiply(firstNumber, secondNumber){
-  return firstNumber * secondNumber;
-}
-
-function divide(firstNumber, secondNumber){
-  return firstNumber / secondNumber;
-}
-
-// console.log(divide(10, 2) * multiply(2, 4) + 3);
-
-function divideThenMultiply(firstNumber, secondNumber){
-  return divide(firstNumber, secondNumber) * multiply(firstNumber, secondNumber);
-}
-
-// console.log(divideThenMultiply(1, 2));
-
-var strOne = "1";
-// strOne = Number(strOne);
-strOne = parseInt(strOne);
-// var numOne = Number(strOne);
-// console.log(typeof strOne);
-console.log(add(strOne, 2));

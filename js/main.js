af108eda1b52a34123f35bd236e809528d579d4e
Isgi Arriza <isgiarriza@gmail.com>
2017-09-06 10:51:21 +0700

    2. Function

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//function atau method
+
+//function dengan langsung cetak ke layar
+// function add(){
+//   console.log('function add dipanggil')
+//   document.getElementById('hello').innerHTML = 'function add dipanggil';
+//   alert('function add dipanggil')
+// }
+
+//cara memanggil method di atas
+//add();
+
+
+//function dengan return value
+// function add(){
+//   return "function add dipanggil";
+// }
+//cara memanggil method di atas
+// alert(add())
+
+//function dengan parameter
+// function add(firstNumber, secondNumber){
+//   return firstNumber + secondNumber;
+// }
+// console.log(add(1, "2"))

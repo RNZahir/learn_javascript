78b6a5ddbf2a468dba96816315feecbb925fac3c
Isgi Arriza <isgiarriza@gmail.com>
2017-09-06 11:43:48 +0700

    5.ifelse comparison

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-//ambil value dari firstNumber
-var firstNumber = document.getElementById('firstNumber')
-var secondNumber = document.getElementById('secondNumber')
+var firstNumber = document.getElementById('firstNumber');
+var secondNumber = document.getElementById('secondNumber');
+var operation = document.getElementById('operation');
 
-function add(){
-  var firstNumberInt = Number(firstNumber.value);
-  var secondNumberInt = Number(secondNumber.value);
-  var result = firstNumberInt + secondNumberInt;
+var isBudiHandsome = true;
+var isYantiBeautiful = false;
 
-  //passing value to dom
-  document.getElementById("result").innerHTML = result;
+// true && true = true
+// true && false = false
+// false && false = false
+// true || false = true
+// false || true = true
+// true || true = true
+// false || false = false
+
+
+function checkOperationAndExecute(){
+  if(isBudiHandsome && isYantiBeautiful){
+    console.log('yes both of them');
+  }else if(isYantiBeautiful == false){
+    console.log('gpp walaupun ga beautiful')
+  }else{
+    console.log('terserah');
+  }
 }
+
+checkOperationAndExecute();
